3fd3a90f9e3e201eed929f970977704803c43675
David Obinna <davidobinna579@gmail.com>
2024-09-19 06:55:45 +0100

    updated

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return inertia('Home');
+        $posts = Post::latest()->paginate(5);
+        return inertia('Home', ['posts' => $posts]);
     }
 
     /**
